76cc260731633cea3c06b9231b57788ed7e29f56
Maf <dragoncatm@gmail.com>
2022-01-25 18:10:50 +0100

    Add small Infos to the left

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
-import { Divider, Grid, Box, useTheme, IconButton } from '@mui/material';
+import { Divider, Grid, Box, useTheme, IconButton, Stack } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 import TimelineComponent from '../components/Timeline';
 import InfoCard from '../components/InfoCard';
 
 import texts from '../components/texts';
 import { ColorModeContext } from './_app';
+import tooltips from '../components/tooltips';
 
 const timeLineMap: string[] = [
   'Mitte 19. Jh. - Anfänge als Arbeiterbewegung',
@@ -23,96 +24,72 @@ const Landing: NextPage = () => {
   const colorMode = React.useContext(ColorModeContext);
 
   return (
-    <React.Fragment>
-      <Grid container columns={20} spacing={6}>
-        <Grid item xs={5}>
+    <Grid container columns={20} spacing={6}>
+      <Grid item xs={5}>
+        <Stack direction="column">
           <TimelineComponent timeLineMap={timeLineMap} />
-        </Grid>
-        <Grid item xs={0.5}>
-          <Divider orientation="vertical" />
-        </Grid>
-        <Grid item xs={14}>
-          <Grid container>
-            <Grid item>
-              <h1>Soziale Frage</h1>
-            </Grid>
-            <Grid item>
-              <Box
-                sx={{
-                  display: 'flex',
-                  width: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  bgcolor: 'background.default',
-                  color: 'text.primary',
-                  borderRadius: 1,
-                  p: 3,
-                }}
-              >
-                {theme.palette.mode} mode
-                <IconButton
-                  sx={{ ml: 1 }}
-                  onClick={colorMode.toggleColorMode}
-                  color="inherit"
-                >
-                  {theme.palette.mode === 'dark' ? (
-                    <Brightness7 />
-                  ) : (
-                    <Brightness4 />
-                  )}
-                </IconButton>
-              </Box>
-            </Grid>
-          </Grid>
-          <Grid container spacing={4} columns={30}>
-            {texts.map((text) => (
-              <Grid item xs={text.size ?? 10}>
-                <InfoCard {...text} variant="outlined" />
+          <Divider orientation="horizontal" />
+          <Grid container ml={3} mt={3}>
+            {Object.keys(tooltips).map((t) => (
+              <Grid item>
+                <InfoCard title={t} text={tooltips[t]} variant="outlined" />
               </Grid>
             ))}
-            <Grid item xs={10}>
-              <InfoCard
-                title="Pauperismus"
-                variant="outlined"
-                imageURL="https://www.hanisauland.de/sites/default/files/styles/article_1180/public/1779513-161311-2020-05/soziale-Frage-kinderarbeit-49823249.jpg.jpeg"
-              >
-                Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h
-                eelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahh Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h
-                eelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahh Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h
-                eelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahh Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h
-                eelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahh Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h
-                eelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
-                heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
-                nahh
-              </InfoCard>
+          </Grid>
+        </Stack>
+      </Grid>
+      <Grid item xs={0.5}>
+        <Divider orientation="vertical" />
+      </Grid>
+      <Grid item xs={14}>
+        <h1>Soziale Frage</h1>
+        <Grid container spacing={4} columns={30}>
+          {texts.map((text) => (
+            <Grid item xs={text.size ?? 10}>
+              <InfoCard {...text} variant="outlined" />
             </Grid>
+          ))}
+          <Grid item xs={10}>
+            <InfoCard
+              title="Pauperismus"
+              variant="outlined"
+              imageURL="https://www.hanisauland.de/sites/default/files/styles/article_1180/public/1779513-161311-2020-05/soziale-Frage-kinderarbeit-49823249.jpg.jpeg"
+            >
+              Oh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h eelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahh Oh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h eelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahh Oh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h eelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahh Oh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h eelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahh Oh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh h eelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh
+              heelll nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll
+              nahhOh heelll nahhOh heelll nahhOh heelll nahhOh heelll nahh
+            </InfoCard>
           </Grid>
         </Grid>
       </Grid>
-    </React.Fragment>
+    </Grid>
   );
 };
 
